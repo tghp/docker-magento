eff6940425c43bb4e8385331ce1e60e5b51657f1
Josh Davenport <josh.davenport@tghp.co.uk>
2019-08-04 18:19:56 +0100

    Added missing mage_run_type fastcgi_param to nginx conf and tweaked formatting

--- images/nginx/1.13/nginx.sh
+++ images/nginx/1.13/nginx.sh
@@ -7,9 +7,9 @@ if [ "$NGINX_HOSTNAME" ]; then
 fi
 
 if [ "$MAGENTO_HOST_RUN_CODES" ]; then
-	MAGENTO_NGINX_RUN_CODE_MAP=$(echo "$MAGENTO_HOST_RUN_CODES" | tr ',' '; ' | tr ':' ' ' )
-	MAGENTO_NGINX_MAP=$(echo "map \$http_host \$MAGE_RUN_CODE { default ''; $MAGENTO_NGINX_RUN_CODE_MAP;}")
-	MAGENTO_NGINX_FASTCGI_MAP='fastcgi_param  MAGE_RUN_CODE $MAGE_RUN_CODE'
+	MAGENTO_NGINX_RUN_CODE_MAP=$(echo "$MAGENTO_HOST_RUN_CODES" | tr ',' ';' | tr ':' ' ' )
+	MAGENTO_NGINX_MAP=$(echo "map \$http_host \$MAGE_RUN_CODE { default '';$MAGENTO_NGINX_RUN_CODE_MAP;}")
+	MAGENTO_NGINX_FASTCGI_MAP='fastcgi_param  MAGE_RUN_CODE $MAGE_RUN_CODE;'
 	f=$(cat /etc/nginx/conf.d/default.conf)
 	f=$(echo "${f/'#nginx_map'/$MAGENTO_NGINX_MAP}")
 	f=$(echo "${f/'#fastcgi_nginx_map'/$MAGENTO_NGINX_FASTCGI_MAP}")
