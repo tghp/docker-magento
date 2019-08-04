ea7d6e8ecc45aa4d36818babebfa5ba50c47e5ba
Josh Davenport <josh.davenport@tghp.co.uk>
2019-08-04 01:41:09 +0100

    Changed shebang

--- images/nginx/1.13/nginx.sh
+++ images/nginx/1.13/nginx.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ "$NGINX_HOSTNAME" ]; then
 	f=$(cat /etc/nginx/conf.d/default.conf)
@@ -7,8 +7,8 @@ if [ "$NGINX_HOSTNAME" ]; then
 fi
 
 if [ "$MAGENTO_HOST_RUN_CODES" ]; then
-	MAGENTO_NGINX_RUN_CODE_MAP=$(echo "$MAGENTO_HOST_RUN_CODES" | tr ',' '\n' | tr ':' ' ' | sed -e 's/$/;/g' | sed -e 's/^/    /g')
-	MAGENTO_NGINX_MAP=$(echo "map \$http_host \$MAGE_RUN_CODE {\n    default '';\n$MAGENTO_NGINX_RUN_CODE_MAP\n}")
+	MAGENTO_NGINX_RUN_CODE_MAP=$(echo "$MAGENTO_HOST_RUN_CODES" | tr ',' '; ' | tr ':' ' ' )
+	MAGENTO_NGINX_MAP=$(echo "map \$http_host \$MAGE_RUN_CODE { default ''; $MAGENTO_NGINX_RUN_CODE_MAP;}")
 	MAGENTO_NGINX_FASTCGI_MAP='fastcgi_param  MAGE_RUN_CODE $MAGE_RUN_CODE'
 	f=$(cat /etc/nginx/conf.d/default.conf)
 	f=$(echo "${f/'#nginx_map'/$MAGENTO_NGINX_MAP}")
