5837d3c40745359e535dbc9850940e16ce485ec4
Josh Davenport <josh.davenport@tghp.co.uk>
2020-07-06 17:14:14 +0100

    Updated nginx shell script as variable substitutions wasn't working

--- images/nginx/1.18/nginx.sh
+++ images/nginx/1.18/nginx.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
+NGINX_CONF=$(cat /etc/nginx/conf.d/default.conf)
+
 if [ "$NGINX_HOSTNAME" ]; then
-	f=$(cat /etc/nginx/conf.d/default.conf)
-	f=$(echo "${f/server_name localhost/server_name $NGINX_HOSTNAME}")
-	echo "$f" > /etc/nginx/conf.d/default.conf
+  NGINX_CONF=$(echo "$NGINX_CONF" | sed "s/server_name localhost/server_name ${NGINX_HOSTNAME}/")
 fi
 
 if [ "$MAGENTO_HOST_RUN_CODES" ]; then
-	MAGENTO_NGINX_RUN_CODE_MAP=$(echo "$MAGENTO_HOST_RUN_CODES" | tr ',' ';' | tr ':' ' ' )
-	MAGENTO_NGINX_MAP=$(echo "map \$http_host \$MAGE_RUN_CODE { default '';$MAGENTO_NGINX_RUN_CODE_MAP;}")
-	MAGENTO_NGINX_FASTCGI_MAP='fastcgi_param  MAGE_RUN_CODE $MAGE_RUN_CODE;'
-	f=$(cat /etc/nginx/conf.d/default.conf)
-	f=$(echo "${f/'#nginx_map'/$MAGENTO_NGINX_MAP}")
-	f=$(echo "${f/'#fastcgi_nginx_map'/$MAGENTO_NGINX_FASTCGI_MAP}")
-	echo "$f" > /etc/nginx/conf.d/default.conf
+  MAGENTO_NGINX_RUN_CODE_MAP=$(echo "$MAGENTO_HOST_RUN_CODES" | tr ',' ';' | tr ':' ' ' )
+  MAGENTO_NGINX_MAP=$(echo "map \$http_host \$MAGE_RUN_CODE { default '';$MAGENTO_NGINX_RUN_CODE_MAP;}")
+  MAGENTO_NGINX_FASTCGI_MAP='fastcgi_param  MAGE_RUN_CODE $MAGE_RUN_CODE;'
+  NGINX_CONF=$(echo "$NGINX_CONF" | sed "s/#nginx_map/${MAGENTO_NGINX_MAP}/")
+  NGINX_CONF=$(echo "$NGINX_CONF" | sed "s/#fastcgi_nginx_map/${MAGENTO_NGINX_FASTCGI_MAP}/")
 fi
+
+echo "$NGINX_CONF" > /etc/nginx/conf.d/default.conf
